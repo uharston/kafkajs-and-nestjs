d62e2fc75e76b18a59d467e1cbcc29f978d86b1c
Uriah Harston <uharston@gmail.com>
2021-04-15 16:34:38 -0500

    Register ClientModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,29 @@
 import { Module } from '@nestjs/common';
+import { ClientsModule, Transport } from '@nestjs/microservices';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { BROKER1, BROKER2, CLIENTID, CLIENT_NAME } from './constants';
 import { MyContController } from './my-cont/my-cont.controller';
 
 @Module({
-  imports: [],
+  imports: [
+    ClientsModule.register([
+      {
+        name: CLIENT_NAME,
+        transport: Transport.KAFKA,
+
+        options: {
+          client: {
+            clientId: CLIENTID,
+            brokers: [BROKER1],
+          },
+          // consumer: {
+          //   groupId: 'hero-consumer',
+          // },
+        },
+      },
+    ]),
+  ],
   controllers: [AppController, MyContController],
   providers: [AppService],
 })
