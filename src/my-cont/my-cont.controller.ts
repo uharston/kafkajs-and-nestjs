353a85f277f82e16f67db85d6e00c76345c9b5ce
Uriah Harston <uharston@gmail.com>
2021-04-15 22:03:36 -0500

    Inject client and subscribe to topic

--- src/my-cont/my-cont.controller.ts
+++ src/my-cont/my-cont.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Inject, OnModuleInit } from '@nestjs/common';
+import { ClientKafka, MessagePattern } from '@nestjs/microservices';
+import { MY_CONT_SERVICE } from 'src/constants';
 
 @Controller('my-cont')
-export class MyContController {}
+export class MyContController implements OnModuleInit {
+  constructor(@Inject(MY_CONT_SERVICE) private client: ClientKafka) {}
+  async onModuleInit() {
+    this.client.subscribeToResponseOf('__consumer_offsets');
+    await this.client.connect();
+  }
+
+  // @MessagePattern('say hello')
+  sayHello() {
+    console.log(this.client);
+    return 'hello';
+  }
+}
