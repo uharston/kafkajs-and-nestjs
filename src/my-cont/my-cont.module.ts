6fbebf0d640f6a1e2f9416a34d7326e9ff7c4cc7
Uriah Harston <uharston@gmail.com>
2021-04-16 16:18:26 -0500

    Remove Kafka config service

--- src/my-cont/my-cont.module.ts
+++ src/my-cont/my-cont.module.ts
@@ -4,24 +4,7 @@ import { BROKER1, CLIENTID, MY_CONT_SERVICE } from 'src/constants';
 import { MyContController } from './my-cont.controller';
 
 @Module({
-  imports: [
-    ClientsModule.register([
-      {
-        name: MY_CONT_SERVICE,
-        transport: Transport.KAFKA,
-        options: {
-          client: {
-            clientId: CLIENTID,
-            brokers: [BROKER1],
-          },
-          consumer: {
-            groupId: 'myCont-consumer',
-          },
-        },
-      },
-    ]),
-    MyContModule,
-  ],
+  imports: [MyContModule],
   controllers: [MyContController],
 })
 export class MyContModule {}
